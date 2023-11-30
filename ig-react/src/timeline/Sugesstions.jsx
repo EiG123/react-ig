c17f957af4181a777eeb1879aac5291d8110284d
EiG123 <narinsuwan567@gmail.com>
2023-11-30 20:55:09 +0700

    update 20:54 30/11/2566

--- ig-react/src/timeline/Sugesstions.jsx
+++ ig-react/src/timeline/Sugesstions.jsx
@@ -1,8 +1,27 @@
+import { Avatar } from "@mui/material";
+import "./Sugesstions.css"
 
 function Suggestions(){
 
     return(
-        <div>Suggestions</div>
+        <div className="suggesstions">
+            <div className="suggestions__title">Suggestions for you</div>
+            <div className="suggestions__usernames">
+                <div className="suggestion__username">
+                    <div className="username__left">
+                        <span className="avatar">
+                            <Avatar>R</Avatar>
+                        </span>
+                        <div className="username__info">
+                            <div className="username">Uwu</div>
+                            <div className="relation">New to Instagram</div>
+                        </div>
+                    </div>
+                    <button className="follow__button">Follow</button>
+
+                </div>
+            </div>
+        </div>
     );
 }
 
