db96a5f54983a759b277e4b749928c288050988a
EiG123 <narinsuwan567@gmail.com>
2023-12-05 11:55:26 +0700

    update 11:55 5/12/2566

--- ig-react/src/authentication/Signup.jsx
+++ ig-react/src/authentication/Signup.jsx
@@ -1,16 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./Signup.css"
 
 function Signup() {
+  const [email, setEmail] = useState("");
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
   return (
     <div className='signup'>
       <img 
         src="https://pngkey.com/png/full/828-8286178_mackeys-work-needs-no-elaborate-presentation-or-distracting.png" 
         alt=""
        />
-      <input type="email" placeholder="Email"/>
-      <input type="text" placeholder="Username" />
-      <input type="password" placeholder="Password" />
+      <input 
+        onChange={e => setEmail(e.target.value)}
+        type="email" 
+        placeholder="Email"
+        value={email}
+      />
+      <input
+        onChange={e => setUsername(e.target.value)} 
+        type="text" 
+        placeholder="Username"
+        value={username} 
+      />
+      <input
+        onChange={e => setPassword(e.target.value)} 
+        type="password" 
+        placeholder="Password"
+        value={password}
+      />
       <button>Log in</button>
     </div>
   )
