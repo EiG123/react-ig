c2eae853355b8b6ff911f14c06c6a61ff9043fe7
EiG123 <narinsuwan567@gmail.com>
2023-12-06 12:25:23 +0700

    update 12:25 6/12/2566

--- ig-react/src/authentication/Signup.jsx
+++ ig-react/src/authentication/Signup.jsx
@@ -1,10 +1,22 @@
 import React, { useState } from 'react'
 import "./Signup.css"
+import {createUserWithEmailAndPassword, signInWithEmailAndPassword, updateProfile} from "firebase/auth"
+import { auth } from '../firebase';
 
 function Signup() {
   const [email, setEmail] = useState("");
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+
+  const handleSignup = () => {
+    createUserWithEmailAndPassword(auth, email, password).then(
+      signInWithEmailAndPassword(auth, email, password).then(
+        updateProfile(auth.currentUser, {displayName: username})
+      )
+    ).catch(err => {
+        alert(err);
+    });
+  };
 
   return (
     <div className='signup'>
@@ -30,7 +42,7 @@ function Signup() {
         placeholder="Password"
         value={password}
       />
-      <button>Log in</button>
+      <button onClick={handleSignup}>Sign Up</button>
     </div>
   )
 }
